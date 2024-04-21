3479825398f8dcad9910540227f540b30393e929
vedaduhan <duhanv@tcd.ie>
2024-04-21 17:04:02 +0100

    changes

--- Data/imagesharing.sql
+++ Data/imagesharing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2024 at 05:11 PM
+-- Generation Time: Apr 21, 2024 at 06:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -44,10 +44,10 @@ CREATE TABLE `uploads` (
 --
 
 INSERT INTO `uploads` (`upload_id`, `upload_file_name`, `upload_path`, `upload_path_org`, `upload_auther_id`, `upload_author`, `upload_description`, `last_updated_on`, `status`) VALUES
-(9, '01dd7b0fa6f', '/uploads/resized/01dd7b0fa6f2aac44ebfbb2d0f7040d2.jpg', '/uploads/01dd7b0fa6f2aac44ebfbb2d0f7040d2.jpg', 0, 'admin', 'ss', '2024-04-14 11:40:05', 1),
-(10, '7386e999a1a', '/uploads/resized/7386e999a1a267c363dda81e467b7ad0.jpg', '/uploads/7386e999a1a267c363dda81e467b7ad0.jpg', 0, 'admin', 'ss', '2024-04-14 12:00:13', 1),
-(11, 'petertuete_', '/uploads/resized/petertuete_Cute_easter_bunny_painting_eggs_disney_c1dc5878-cfe2-4990-9514-bd0ffd789d42.png', '/uploads/petertuete_Cute_easter_bunny_painting_eggs_disney_c1dc5878-cfe2-4990-9514-bd0ffd789d42.png', 0, 'yash', 'B', '2024-04-14 12:08:28', 1),
-(12, 'out_0 (1).p', '/uploads/resized/out_0 (1).png', '/uploads/out_0 (1).png', 0, 'yash', 'l', '2024-04-14 12:29:12', 1),
+(9, '01dd7b0fa6f', '/uploads/resized/01dd7b0fa6f2aac44ebfbb2d0f7040d2.jpg', '/uploads/01dd7b0fa6f2aac44ebfbb2d0f7040d2.jpg', 0, 'Alex', 'ss', '2024-04-14 11:40:05', 1),
+(10, '7386e999a1a', '/uploads/resized/7386e999a1a267c363dda81e467b7ad0.jpg', '/uploads/7386e999a1a267c363dda81e467b7ad0.jpg', 0, 'Chiara', 'ss', '2024-04-14 12:00:13', 1),
+(11, 'petertuete_', '/uploads/resized/petertuete_Cute_easter_bunny_painting_eggs_disney_c1dc5878-cfe2-4990-9514-bd0ffd789d42.png', '/uploads/petertuete_Cute_easter_bunny_painting_eggs_disney_c1dc5878-cfe2-4990-9514-bd0ffd789d42.png', 0, 'Sally', 'B', '2024-04-14 12:08:28', 1),
+(12, 'out_0 (1).p', '/uploads/resized/out_0 (1).png', '/uploads/out_0 (1).png', 0, 'Elliot', 'l', '2024-04-14 12:29:12', 1),
 (13, '8-hqnG0rDu2', '/uploads/resized/8-hqnG0rDu2v2TmYF.png', '/uploads/8-hqnG0rDu2v2TmYF.png', 0, 'Veda', 'something new', '2024-04-17 15:37:17', 1);
 
 -- --------------------------------------------------------
@@ -70,8 +70,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `username`, `email`, `password`, `created_on`, `last_updated_on`) VALUES
-(5, 'admin', 'admn@1', '$2b$10$OptQHfMmzMv4uer7JNampe.mjpCqDL8sXX/.2hnLqkIQ8XcmBGfG2', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(6, 'yash', 'yash@1', '$2b$10$/tXmTvo3QYVTT5upUR3jRuGS3ykLQrzCgh7UbB7esj7ioh4P9aAhG', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 'Sally', 'sally@gmail.com', '$2b$10$OptQHfMmzMv4uer7JNampe.mjpCqDL8sXX/.2hnLqkIQ8XcmBGfG2', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'Alex', 'alex@gmail.com', '$2b$10$/tXmTvo3QYVTT5upUR3jRuGS3ykLQrzCgh7UbB7esj7ioh4P9aAhG', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (7, 'Veda', 'duhanv@tcd.ie', '$2b$10$rctI9GKvajPAA/t615QTAuZ95haTnggbsk7vfCdI78m911jpEo4e.', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
